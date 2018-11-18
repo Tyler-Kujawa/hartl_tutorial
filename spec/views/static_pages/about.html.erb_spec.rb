ec1f8fa00b657944d8b4873fc41ae7f16e95ab83
TylerKujawa <tyler.kujawa@teamnortwoods.com>
2018-11-18 17:10:25 -0500

    Adding guard file clearning up view specs

--- spec/views/static_pages/about.html.erb_spec.rb
+++ spec/views/static_pages/about.html.erb_spec.rb
@@ -3,6 +3,6 @@ require "rails_helper"
 RSpec.describe "static_pages/about.html.erb", type: :view do
   it "displays the correct title" do
     render
-    rendered.should have_selector("h1", text: "About us")
+    rendered.should have_selector("h1", text: "About us", exact: true)
   end
 end
--- spec/views/static_pages/help.html.erb_spec.rb
+++ spec/views/static_pages/help.html.erb_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe "static_pages/help.html.erb", type: :view do
   it "displays the correct header" do
     render
-    rendered.should have_selector("h1", text: "Help")
+    rendered.should have_selector("h1", text: "Help", exact: true)
   end
 end
