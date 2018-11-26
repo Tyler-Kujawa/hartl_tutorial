278b1f2ed7a8b851b2e07c223491a1c91cf20429
TylerKujawa <tyler.kujawa@teamnortwoods.com>
2018-11-25 22:20:57 -0500

    Adding partial for head content

--- spec/views/static_pages/home.html.erb_spec.rb
+++ spec/views/static_pages/home.html.erb_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe "static_pages/home.html.erb", type: :view do
   it "displays the right title" do
     render
-    rendered.should have_selector("h1", text: "Home page", exact: true) 
+    rendered.should have_selector("h1", text: "Welcome to the Sample App", exact: true) 
   end
 end
