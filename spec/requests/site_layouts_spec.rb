1024140b492f37bf2872d8ae616895533df73e22
TylerKujawa <tyler.kujawa@teamnortwoods.com>
2018-11-25 23:39:52 -0500

    Adding missed files

---
+++ spec/requests/site_layouts_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "SiteLayouts", type: :request do
+  describe "GET /site_layouts" do
+    subject { get root_path }
+
+    it "renders the home template for the root route" do
+      expect(subject).to render_template(:home)
+    end
+  end
+end
