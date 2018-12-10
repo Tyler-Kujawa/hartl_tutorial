05df4a27a74056fd6f5f64cb8c93f0e49284f52d
TylerKujawa <tyler.kujawa@teamnortwoods.com>
2018-12-09 20:37:13 -0500

    adding integration tests for header links

---
+++ spec/features/header_links_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'the header' do
+  before :each do
+    visit root_path
+  end
+
+  it 'displays the correct links' do
+    expect(page).to have_content('Home')
+    expect(page).to have_content('Help')
+    expect(page).to have_content('Contact')
+    expect(page).to have_content('Log in')
+  end
+
+  describe 'routes the user to the correct page' do
+    it 'goes to help' do
+      click_link 'Help'
+      expect(page).to have_current_path help_path
+    end
+
+    it 'goes to contact' do
+      click_link 'Contact', match: :first
+      expect(page).to have_current_path contact_path
+    end
+
+    xit 'goes to Log in' do
+      click_link 'Log in'
+      expect(page).to have_current_path login_path
+    end
+  end
+end
