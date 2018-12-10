cb6696608a8e40f3462709d44bff678cf13ebde0
TylerKujawa <tyler.kujawa@teamnortwoods.com>
2018-12-09 19:57:34 -0500

    Adding named routes for static_pages

--- spec/routing/general_routing_spec.rb
+++ spec/routing/general_routing_spec.rb
@@ -4,4 +4,16 @@ RSpec.describe "General Routing", type: :routing do
   it "routes root path to static_content#home" do
     expect(get("/")).to route_to(controller: "static_pages", action: "home")
   end
+
+  it 'routes about_path to static_content#about' do
+    expect(get: about_path).to route_to(controller: "static_pages", action: "about")
+  end
+
+  it 'routes about_path to static_content#help' do
+    expect(get: help_path).to route_to(controller: "static_pages", action: "help")
+  end
+
+  it 'routes about_path to static_content#contact' do
+    expect(get: contact_path).to route_to(controller: "static_pages", action: "contact")
+  end
 end
