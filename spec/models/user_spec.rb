07615c62b8d798a459378419f4fc205167d2d4ce
TylerKujawa <tyler.kujawa@teamnortwoods.com>
2018-12-09 21:53:58 -0500

    adding constraints for name and email to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new name: 'coolUser', email: 'coolUser@cool.com'}
+
+  describe 'validations' do
+    it 'is a valid user' do
+      expect(user.valid?).to be(true)
+    end
+
+    it 'name is not present' do
+      user.name = nil
+      expect(user.valid?).to be(false)
+    end
+
+    it 'email is not present' do
+      user.email = nil
+      expect(user.valid?).to be(false)
+    end
+
+    xit 'email is not formatted correctly' do
+      user.email = 'this is an email'
+      expect(user.valid?).to be(false)
+    end
+  end
 end
