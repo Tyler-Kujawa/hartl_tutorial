cedd8417df55e16a8eec507678897179bf0df101
TylerKujawa <tyler.kujawa@teamnortwoods.com>
2018-12-10 23:05:58 -0500

    cleaning up user spec by using subject

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,34 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { User.new name: 'coolUser', email: 'coolUser@cool.com'}
+  let(:user) { User.new name: 'coolUser', email: 'coolUser@cool.com' }
+  subject { user.valid? }
 
   describe 'validations' do
-    it 'is a valid user' do
-      expect(user.valid?).to be(true)
-    end
+    it { is_expected.to be true }
 
     describe 'name' do
       it 'is not present' do
         user.name = nil
-        expect(user.valid?).to be(false)
+        is_expected.to be(false)
       end
 
       it 'is too long' do
         user.name = "#{'a' * 52}"
-        expect(user.valid?).to be(false)
+        is_expected.to be(false)
       end
     end
 
     describe 'email' do
       it 'is not present' do
         user.email = nil
-        expect(user.valid?).to be(false)
+        is_expected.to be(false)
       end
 
       it 'it is too long' do
         user.email = "#{'a' * 260}@gmail.com"
-        expect(user.valid?).to be(false)
+        is_expected.to be(false)
       end
     end
   end
