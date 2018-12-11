91cddb21b32cf19c1249f2ed468ebc9487e604f2
TylerKujawa <tyler.kujawa@teamnortwoods.com>
2018-12-10 22:40:43 -0500

    Adding max length constraint to name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,9 +8,16 @@ RSpec.describe User, type: :model do
       expect(user.valid?).to be(true)
     end
 
-    it 'name is not present' do
-      user.name = nil
-      expect(user.valid?).to be(false)
+    describe 'name' do
+      it 'is not present' do
+        user.name = nil
+        expect(user.valid?).to be(false)
+      end
+
+      it 'is too long' do
+        user.name = "#{'a' * 52}"
+        expect(user.valid?).to be(false)
+      end
     end
 
     it 'email is not present' do
